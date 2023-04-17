66533985ca2abe38b28874838ffb05f012cbf45a
gavin <gavinsutton6511@gmail.com>
2023-04-17 17:13:38 +0900

    Finish 08_calculator js assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,29 +3,41 @@ const add = function(a,b) {
   return result;
 };
 
-const subtract = function(a, b) {
+const subtract = function(a,b) {
   let result = a-b;
   return result;
 };
 
-const sum = function() {
-	let result = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    result += arguments[i];
+const sum = function(args) {
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    result += args[i];
   }
   return parseInt(result);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  let result = 1;
+  for (let i = 0; i < args.length; i++) {
+    result *= args[i];
+  }
+  return parseInt(result);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let num = a;
+  let power = b;
+  let result = num ** power;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let num = a;
+  let result = 1;
+  for (let i = 0; i < num; i++) {
+    result *= num-i;
+  }
+  return result;
 };
 
 // Do not edit below this line
