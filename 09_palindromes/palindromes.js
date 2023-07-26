62853a6be1a981a25995e853d91b6461c5db733f
gavin <gavinsutton6511@gmail.com>
2023-07-26 15:28:19 +0900

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let replaced = words.replace(/[^a-z0-9]/gi, '').toLowerCase(); //removes non-alphanumeric characters
+    let reversed = replaced.split("").reverse().join("");
+    return (replaced === reversed ? true : false);
 };
 
 // Do not edit below this line
