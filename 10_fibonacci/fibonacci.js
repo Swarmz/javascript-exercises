56d0ab4819f4f2b16be712e708d578dbeaf2eeda
gavin <gavinsutton6511@gmail.com>
2023-08-01 17:39:42 +0900

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
 
+const fibonacci = function(fibNum) {
+    Number(fibNum);
+    let myArr = new Array(2).fill(1);
+    if (fibNum > 0) {
+        for (let i=1; i<fibNum; i++){
+                let nextNum = 0;
+                nextNum = myArr[i]+myArr[i-1];
+                myArr.push(nextNum);
+            }
+        return myArr[fibNum-1];
+    }
+    else {
+        return "OOPS";
+    }   
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
