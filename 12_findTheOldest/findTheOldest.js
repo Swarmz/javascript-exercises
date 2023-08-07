f9125f2964c05729e69b589d2d133c7656908ae1
gavin <gavinsutton6511@gmail.com>
2023-08-07 18:41:19 +0900

    Finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const dateYear = new Date().getFullYear();
+    function addYearOfDeath(people) { // If yearofDeath key doesn't exist, add it and assign current year as value
+        if (!("yearOfDeath" in people) == true) { 
+            people.yearOfDeath = dateYear;
+        }
+    }
+    people.forEach(addYearOfDeath);
+    const oldestPerson = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
